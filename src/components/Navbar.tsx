8957b3cd9d7534131839496f7410ed01d8a39319
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2025-11-28 20:25:21 -0500

    Fix path menu to navigate as SPA portfolio

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Tooltip } from "./Tooltip";
-import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export function Navbar() {
@@ -10,9 +9,12 @@ export function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
   const toggleButtonRef = useRef<HTMLButtonElement>(null);
+  const location = useLocation();
 
   // Theme state
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  const headerRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
     document.documentElement.classList.add("theme-ready");
@@ -33,13 +35,26 @@ export function Navbar() {
     }, 350);
   }
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        headerRef.current?.classList.add("navbar-scrolled");
+      } else {
+        headerRef.current?.classList.remove("navbar-scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   const MENU_ITEMS = [
-    { id: "home", path: "/", label: "navbar.home" },
-    { id: "about", path: "/about", label: "navbar.about" },
-    { id: "experience", path: "/experience", label: "navbar.experience" },
-    { id: "technologies", path: "/technologies", label: "navbar.technologies" },
-    { id: "project", path: "/project", label: "navbar.project" },
-    { id: "contact", path: "/contact", label: "navbar.contact" },
+    { id: "home", path: "#home", label: "navbar.home" },
+    { id: "about", path: "#about", label: "navbar.about" },
+    { id: "experience", path: "#experience", label: "navbar.experience" },
+    { id: "technologies", path: "#technologies", label: "navbar.technologies" },
+    { id: "project", path: "#project", label: "navbar.project" },
+    { id: "contact", path: "#contact", label: "navbar.contact" },
   ] as const;
 
   // Language options for your switcher and names for toast messages or UI labels
@@ -56,6 +71,36 @@ export function Navbar() {
       document.body.style.overflow = "";
     };
   }, [isOpen]);
+
+  // Observe sections to update hash and focus navbar link on enter viewport
+  useEffect(() => {
+    const sections = MENU_ITEMS.map((item) =>
+      document.getElementById(item.id)
+    ).filter((el): el is HTMLElement => el !== null);
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            history.replaceState(null, "", `#${entry.target.id}`);
+
+            const menuLink = document.querySelector(
+              `nav a[href="#${entry.target.id}"]`
+            );
+            (menuLink as HTMLElement)?.focus();
+          }
+        });
+      },
+      {
+        threshold: 0.6,
+        rootMargin: "-70px 0px -30% 0px",
+      }
+    );
+
+    sections.forEach((section) => observer.observe(section));
+
+    return () => observer.disconnect();
+  }, []);
 
   // Handle outside click, touch, tab key and Escape key
   useEffect(() => {
@@ -97,12 +142,21 @@ export function Navbar() {
   }, []);
 
   return (
-    <header className="fixed text-(--color-text-navbar) top-0 left-0 w-full z-50 bg-(--color-bg-navbarAndFooter) shadow-md">
+    <header
+      ref={headerRef}
+      className="fixed text-(--color-text-navbar) top-0 left-0 w-full z-50 bg-(--color-bg-navbarAndFooter) shadow-md navbar-scroll"
+    >
       <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
         {/* Logo */}
         <Tooltip labelKey="navbar.tooltipLogo">
           <Link
             to="/"
+            onClick={(e) => {
+              e.preventDefault(); // prevent route navigation
+              document
+                .getElementById("home") // find your home section
+                ?.scrollIntoView({ block: "start", behavior: "smooth" }); // scroll to its start
+            }}
             aria-label={t("aria.logo")}
             className="inline-block focus:outline-none
       focus-visible:ring-2 focus-visible:ring-[#d2ad4b]
@@ -119,28 +173,39 @@ export function Navbar() {
 
         {/* Desktop menu */}
         <nav className="hidden md:flex gap-6" aria-label={t("aria.mainNav")}>
-          {MENU_ITEMS.map(({ id, path, label }) => (
-            <NavLink
-              key={id}
-              to={path}
-              end={path === "/"}
-              aria-label={t(`navbar.alt.${id}`)}
-              className={({ isActive }: { isActive: boolean }) =>
-                `relative text-sm lg:text-2xl transition focus:outline-none
-         hover:after:w-full focus-visible:after:w-full
-         after:absolute after:-bottom-1 after:left-0 after:h-1
-         after:bg-(--color-hover-text-navbarAndFooter) after:w-0
-         after:transition-all after:duration-300
-         ${
-           isActive
-             ? "after:w-full text-(--color-text-navbar)"
-             : "text-(--color-text-navbar)"
-         }`
-              }
-            >
-              {t(label)}
-            </NavLink>
-          ))}
+          {MENU_ITEMS.map(({ id, path, label }) => {
+            // Detect if current scroll-section is the same as menu hash
+            const isActive = location.hash === path;
+
+            const handleClick = (e: React.MouseEvent) => {
+              e.preventDefault(); // avoid navigation jump
+              const section = document.getElementById(id);
+              section?.scrollIntoView({ behavior: "smooth", block: "start" });
+            };
+
+            return (
+              <a
+                key={id}
+                href={path}
+                onClick={handleClick}
+                aria-label={t(`navbar.alt.${id}`)}
+                className={`
+        relative text-l lg:text-2xl transition focus:outline-none
+        hover:after:w-full focus-visible:after:w-full
+        after:absolute after:-bottom-1 after:left-0 after:h-1
+        after:bg-(--color-hover-text-navbarAndFooter) after:w-0
+        after:transition-all after:duration-300
+        ${
+          isActive
+            ? "after:w-full text-(--color-text-navbar)"
+            : "text-(--color-text-navbar)"
+        }
+      `}
+              >
+                {t(label)}
+              </a>
+            );
+          })}
         </nav>
 
         {/* Mobile toggle */}
@@ -241,32 +306,43 @@ export function Navbar() {
           {/* Drawer */}
           <div
             ref={modalRef}
-            className="w-2/3 max-w-sm bg-(--color-bg-navbarAndFooter) h-full shadow-lg p-6 flex flex-col gap-6 text-lg relative"
+            className="w-2/3 max-w-sm bg-(--color-bg-navbarAndFooter) h-full shadow-lg p-6 flex flex-col gap-6 text-lg relative pt-18"
           >
             {/* Menu mobile items */}
-            {MENU_ITEMS.map(({ id, path, label }) => (
-              <NavLink
-                key={id}
-                to={path}
-                end={path === "/"}
-                aria-label={t(`navbar.alt.${id}`)}
-                onClick={() => setIsOpen(false)}
-                className={({ isActive }: { isActive: boolean }) =>
-                  `relative inline-block w-fit text-lg font-semibold transition
-       hover:after:w-full focus:outline-none focus-visible:after:w-full
-       after:absolute after:-bottom-1 after:left-0 after:h-1
-       after:bg-(--color-hover-text-navbarAndFooter) after:w-0
-       after:transition-all after:duration-300
-       ${
-         isActive
-           ? "after:w-full text-(--color-text-navbar)"
-           : "text-(--color-text-navbar)"
-       }`
-                }
-              >
-                {t(label)}
-              </NavLink>
-            ))}
+            {MENU_ITEMS.map(({ id, path, label }) => {
+              // Active if hash matches the menu path
+              const isActive = location.hash === path;
+
+              const handleClick = (e: React.MouseEvent) => {
+                e.preventDefault();
+                const section = document.getElementById(id);
+                section?.scrollIntoView({ behavior: "smooth", block: "start" });
+                setIsOpen(false); // Close mobile drawer AFTER scrolling
+              };
+
+              return (
+                <a
+                  key={id}
+                  href={path}
+                  aria-label={t(`navbar.alt.${id}`)}
+                  onClick={handleClick} // trigger scroll + close menu
+                  className={`
+        relative inline-block w-fit text-lg font-semibold transition focus:outline-none
+        hover:after:w-full focus-visible:after:w-full
+        after:absolute after:-bottom-1 after:left-0 after:h-1
+        after:bg-(--color-hover-text-navbarAndFooter) after:w-0
+        after:transition-all after:duration-300
+        ${
+          isActive
+            ? "after:w-full text-(--color-text-navbar)"
+            : "text-(--color-text-navbar)"
+        }
+      `}
+                >
+                  {t(label)}
+                </a>
+              );
+            })}
 
             {/* Mobile language selector */}
             <div className="flex justify-center space-x-4 pt-6 border-t border-gray-200">
